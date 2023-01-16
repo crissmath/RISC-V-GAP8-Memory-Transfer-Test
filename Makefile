14a0cd232bc989f26516f78ed69023008ffbf37e
crissmath <crirabe@upv.es>
2023-01-16 19:51:26 +0100

    Update last test memory copies

--- Makefile
+++ Makefile
@@ -1,20 +1,17 @@
 #------------------------------------
-
 # t_mem_L3_L2  	  = test_memory_ram_L3_L2
 # t_mem_L2_L3  	  = test_memory_ram_L2_L3
 # t_mem_L3_L3  	  = test_memory_ram_L3_L3
 # t_mem_L3_L1_fc  = test_memory_ram_L3_L1_fc
 # t_mem_L3_L1_cl  = test_memory_ram_L3_L1_cl
 # t_mem_L3_Reg 	  = test_memory_ram_L3_Reg
-TYPE = -Dt_mem_L3_Reg
-
+TYPE = -Dt_mem_L3_L1_cl
 APP              = test_mem
 APP_SRCS        += test_mem_cp.c 
 APP_CFLAGS      += -O2 $(TYPE)
 
 # rules for old api 
 #include $(GAP_SDK_HOME)/tools/rules/pmsis_rules.mk
-
 # new api
 include $(RULES_DIR)/pmsis_rules.mk
 
--- test_mem_cp.c
+++ test_mem_cp.c
@@ -15,7 +15,7 @@
 #define reps (1000)
 #define BUFFER_SIZE_L2 (mc * nc)
 #define BUFFER_SIZE_L3 (mc * nc)
-#define VERBOSE
+//#define VERBOSE
 
 // 0: L3 --> L2
 // 1: L2---> L3
@@ -36,6 +36,18 @@ signed char *buff_L1_cl;  // L1 inside of cluster
 // Ram config parameters
 static struct pi_device ram;          // ram object
 static struct pi_hyperram_conf conf;  // config object
+
+// struct for arguments to transfers
+typedef struct trans_cl_arg {
+  signed char *L3_buff;
+  signed char *L2_buff;
+  signed char *L2_aux;
+  signed char *L1_buff_cl;
+  unsigned int ram;
+  unsigned int b;
+} trans_cl_arg_t;
+
+trans_cl_arg_t cl_Arg;
 
 /***************************************************
  *                 Transfers L3->L2
@@ -101,7 +113,6 @@ void test_memory_ram_L3_L2(void) {
 #ifdef VERBOSE
   printf("done:Copy..\n");
 #endif
-
   /*-------free memory----------------------*/
   pmsis_l2_malloc_free(buff_L2, (uint32_t)BUFFER_SIZE_L2);
   pi_ram_free(&ram, (uint32_t)buff_L3, (uint32_t)BUFFER_SIZE_L3);
@@ -290,7 +301,7 @@ void test_memory_ram_L3_L1_fc(void) {
     pmsis_exit(-4);
   }
   // Buffer L1
-  buff_L1_fc = (signed char *)pmsis_fc_tcdm_malloc((uint32_t)BUFFER_SIZE_L2);
+  buff_L1_fc = (signed char *)pi_fc_l1_malloc((uint32_t)BUFFER_SIZE_L2);
 #ifdef VERBOSE
   printf("done:alloc buffs..\n");
 #endif
@@ -318,6 +329,7 @@ void test_memory_ram_L3_L1_fc(void) {
 #endif
 
   /*-------free memory----------------------*/
+  // pi_fc_l1_malloc_free(buff_L1_fc, (uint32_t)BUFFER_SIZE_L2);
   pmsis_l2_malloc_free(buff_L2, (uint32_t)BUFFER_SIZE_L2);
   pmsis_l2_malloc_free(buff_aux, (uint32_t)BUFFER_SIZE_L2);
   pi_ram_free(&ram, (uint32_t)buff_L3, (uint32_t)BUFFER_SIZE_L3);
@@ -328,49 +340,41 @@ void test_memory_ram_L3_L1_fc(void) {
 }
 
 /***************************************************
- *                 Transfers L3->L1 cl
+ *                 Transfers L3->L1_cl
  **************************************************/
 /*! \brief This function implement transfer L3->L1
  * L1 in the cluster
  **************************************************/
-void test_memory_ram_L3_L1_cl(void) {
-#ifdef VERBOSE
-  printf("start:ram init..\n");
-#endif
-  /*------------Init memory----------------- */
-  /* Init & open ram. */
-  pi_hyperram_conf_init(&conf);
-  pi_open_from_conf(&ram, &conf);
-  if (pi_ram_open(&ram)) {
-    printf("Error ram open !\n");
-    pmsis_exit(-3);
-  }
-#ifdef VERBOSE
-  printf("done:ram init..\n");
-#endif
-
-  float t1, t2, t, time;
-  uint32_t errors = 0;
-
-#ifdef VERBOSE
-  printf("start:alloc buffs..\n");
-#endif
-
-#ifdef VERBOSE
-  printf("done:alloc buffs..\n");
-#endif
-
-#ifdef VERBOSE
+void test_memory_ram_L3_L1_cl(trans_cl_arg_t *cl_Arg) {
+#ifdef VERBOSE_buf_cl
   printf("start:transfers..\n");
 #endif
-  // write n_cp blocks in external ram
+  // write n_cp blocks from external ram to L1_cl
 
-#ifdef VERBOSE
-  printf("done:transfers..\n");
+  signed char *buff_L3_cl = cl_Arg->L3_buff;
+  signed char *buff_L2_cl = cl_Arg->L2_buff;
+  signed char *buff_aux_cl = cl_Arg->L2_aux;
+  signed char *buff_L1_cl = cl_Arg->L1_buff_cl;
+  unsigned int ram = cl_Arg->ram;
+
+#ifdef VERBOSE_buff_cl
+  printf("L3 = %x\n", buff_L3_cl);
+  printf("L2 = %x\n", buff_L2_cl);
+  printf("aux = %x\n", buff_aux_cl);
+  printf("L1_cl = %x\n", buff_L1_cl);
+  printf("ram = %x\n", ram);
 #endif
 
-#ifdef VERBOSE
-  printf("done:Memfree..\n");
+  int b = 10;
+  pi_cl_ram_req_t req_cp;
+  pi_cl_ram_copy(ram, (uint32_t)(buff_L3_cl), buff_aux_cl, (uint32_t)b, 1,
+                 &req_cp);
+  pi_cl_ram_copy_wait(&req_cp);
+  // for (int j = 0; j < b; j++) {
+  //  buff_L1_cl[j] = buff_aux[j];
+  //}
+#ifdef VERBOSE_buff_cl
+  printf("done:transfers..\n");
 #endif
 }
 
@@ -417,7 +421,7 @@ void test_memory_ram_L3_Reg(void) {
   printf("start:transfers..\n");
 #endif
   // write n_cp blocks in external ram
-  for (int b = 1; b < 16; b++) {
+  for (int b = 1; b < (mc * nc); b++) {
     t1 = rt_time_get_us();
     for (int i = 0; i < reps; i++) {
       pi_ram_copy(&ram, (uint32_t)(buff_L3), Reg, (uint32_t)b, 1);
@@ -439,12 +443,57 @@ void test_memory_ram_L3_Reg(void) {
 #endif
 }
 
+/************************************************************
+ *               Delegate function
+ *  In this function we are in the core0(master) of the cluster
+ *  The cluster controller is core 0 of the cluster.
+ *  Core 1 to 7 are the slave cores.
+ *************************************************************/
+void cluster_delegate(void *arg) {
+  /*In this function we are in the core 0 of the cluster*/
+#ifdef VERBOSE_buff_cl
+  printf("Cluster master core entry\n");
+#endif
+  int nb_cores = 1;
+  pi_cl_team_fork(nb_cores, (void *)test_memory_ram_L3_L1_cl, arg);
+#ifdef VERBOSE_buf_cl
+  printf("Cluster master core exit\n");
+#endif
+}
+
 /*************************************************
- *                 Cluster Config
+ *                 FC MAIN
  **************************************************/
-/*! \brief Config cluster && open
+/*! \brief This function is the fabric controller
+ * entry program.
  **************************************************/
-void cluster_config() {
+void fc_main() {
+#ifdef VERBOSE
+  printf("Entering Fabric Controller\n");
+#endif
+  //----- Choose the type of transfer you need to test
+  // t_mem_L3_L2      = test_memory_ram_L3_L2
+  // t_mem_L2_L3      = test_memory_ram_L2_L3
+  // t_mem_L3_L3      = test_memory_ram_L3_L3
+  // t_mem_L3_L1_fc   = test_memory_ram_L3_L1_fc
+  // t_mem_L3_L1_cl   = test_memory_ram_L3_L1_cl
+  // t_mem_L3_Reg     = test_memory_ram_L3_Reg
+#ifdef t_mem_L3_L2
+  test_memory_ram_L3_L2();
+
+#elif defined(t_mem_L2_L3)
+  test_memory_ram_L2_L3();
+
+#elif defined(t_mem_L3_L3)
+  test_memory_ram_L3_L3();
+
+#elif defined(t_mem_L3_L1_fc)
+  test_memory_ram_L3_L1_fc();
+
+#elif defined(t_mem_L3_L1_cl)
+
+  // in this function we need to configure the cluster
+  // cluster_config();
   // Cluster-configuration----
   uint32_t core_id = pi_core_id();
   uint32_t cluster_id = pi_cluster_id();
@@ -469,81 +518,109 @@ void cluster_config() {
   // Set the max freq for the cluster @1.2V
   uint32_t cl_freq_in_Hz = 175 * 1000 * 1000;
   pi_freq_set(PI_FREQ_DOMAIN_CL, cl_freq_in_Hz);
-}
 
-/************************************************************
- *               Delegate function
- *  In this function we are in the core0(master) of the cluster
- *  The cluster controller is core 0 of the cluster.
- *  Core 1 to 7 are the slave cores.
- *************************************************************/
-void cluster_delegate(void *arg) {
-  /*In this function we are in the core 0 of the cluster*/
 #ifdef VERBOSE
-  printf("Cluster master core entry\n");
+  printf("start:ram init..\n");
 #endif
-  printf("Run Task\n");
-  uint nb_cores = 1;
-
-  printf("Run mem copy L3->L1..\n");
-  pi_cl_team_fork(nb_cores, (void *)test_memory_ram_L3_L1_cl, arg);
-}
-
-/*************************************************
- *                 FC MAIN
- **************************************************/
-/*! \brief This function is the fabric controller
- * entry program.
- **************************************************/
-void fc_main() {
+  //------------Init memory-----------------
+  // Init & open ram
+  pi_hyperram_conf_init(&conf);
+  pi_open_from_conf(&ram, &conf);
+  if (pi_ram_open(&ram)) {
+    printf("Error ram open !\n");
+    pmsis_exit(-3);
+  }
 #ifdef VERBOSE
-  printf("Entering Fabric Controller\n");
+  printf("done:ram init..\n");
 #endif
-  //        Choose the option you need
-  //----- Choose the type of transfer you need to test
-  // t_mem_L3_L2      = test_memory_ram_L3_L2
-  // t_mem_L2_L3      = test_memory_ram_L2_L3
-  // t_mem_L3_L3      = test_memory_ram_L3_L3
-  // t_mem_L3_L1_fc   = test_memory_ram_L3_L1_fc
-  // t_mem_L3_L1_cl   = test_memory_ram_L3_L1_cl
-  // t_mem_L3_Reg     = test_memory_ram_L3_Reg
+// ---------- Alloc Buffers ---------------
+#ifdef VERBOSE
+  printf("start:alloc buffs..\n");
+#endif
+  // Buff L3
+  if (pi_ram_alloc(&ram, &buff_L3, (uint32_t)mc * nc)) {
+    printf("Ram malloc failed A!\n");
+    pmsis_exit(-4);
+  }
+  // Buff L2
+  buff_L2 = (signed char *)pmsis_l2_malloc((uint32_t)BUFFER_SIZE_L2);
+  if (buff_L2 == NULL) {
+    printf("buff_L2 alloc failed !\n");
+    pmsis_exit(-1);
+  }
+  // Buff aux(L2)
+  buff_aux = (signed char *)pmsis_l2_malloc((uint32_t)BUFFER_SIZE_L2);
+  if (buff_aux == NULL) {
+    printf("buff_L2 alloc failed !\n");
+    pmsis_exit(-2);
+  }
+  // Buffer L1_cl
+  buff_L1_cl = (signed char *)pmsis_l1_malloc((uint32_t)BUFFER_SIZE_L2);
+#ifdef VERBOSE
+  printf("done:alloc buffs..\n");
+#endif
 
-#ifdef t_mem_L3_L2
-  test_memory_ram_L3_L2();
+#ifdef VERBOSE_buf_cl
+  printf("L3 = %x\n", buff_L3);
+  printf("L2 = %x\n", buff_L2);
+  printf("aux = %x\n", buff_aux);
+  printf("L1_cl = %x\n", buff_L1_cl);
+  printf("ram = %x\n", ram);
+#endif
 
-#elif defined(t_mem_L2_L3)
-  test_memory_ram_L2_L3();
+  // assig arguments
+  cl_Arg.L3_buff = buff_L3;
+  cl_Arg.L2_buff = buff_L2;
+  cl_Arg.L2_aux = buff_aux;
+  cl_Arg.L1_buff_cl = buff_L1_cl;
+  cl_Arg.ram = &ram;
 
-#elif defined(t_mem_L3_L3)
-  test_memory_ram_L3_L3();
-
-#elif defined(t_mem_L3_L1_fc)
-  test_memory_ram_L3_L1_fc();
-
-#elif defined(t_mem_L3_L1_cl)
-
-  // in this function we need to configure the cluster
-  cluster_config();
   //------- Task setup -------------
   struct pi_cluster_task cl_task = {0};
   cl_task.entry = cluster_delegate;
-  // cl_task.arg = (void *)&cl_Arg;
   //---send task to cluster------
-  pi_cluster_send_task_to_cl(&cluster_dev, &cl_task);
+  // timer variables
+  float t1, t2, t, time;
+  uint32_t errors = 0;
+#ifdef VERBOSE
+  printf("send task to cluster..\n");
+#endif
+  for (int b = 1; b < (mc * nc); b++) {
+    t1 = rt_time_get_us();
+    for (int i = 0; i < reps; i++) {
+      cl_Arg.b = b;
+      cl_task.arg = (void *)&cl_Arg;
+      pi_cluster_send_task_to_cl(&cluster_dev, &cl_task);
+    }
+    t2 = rt_time_get_us();
+    t = (t2 - t1) / 1e6;
+    time = (t / reps);
+    printf("%d %f %f\n", b, time, b / time);
+  }
   printf("Close cluster after end of computation.\n");
   pi_cluster_close(&cluster_dev);
+
+  /*-------free memory----------------------*/
+  pmsis_l1_malloc_free(buff_L1_cl, (uint32_t)BUFFER_SIZE_L2);
+  pmsis_l2_malloc_free(buff_L2, (uint32_t)BUFFER_SIZE_L2);
+  pmsis_l2_malloc_free(buff_aux, (uint32_t)BUFFER_SIZE_L2);
+  pi_ram_free(&ram, (uint32_t)buff_L3, (uint32_t)BUFFER_SIZE_L3);
+  pi_ram_close(&ram);
+#ifdef VERBOSE
+  printf("done:Memfree..\n");
+#endif
 
 #elif defined(t_mem_L3_Reg)
   test_memory_ram_L3_L3();
 #else
   continue;
 #endif
+  pmsis_exit(0);
 }
 
-int main(void) {
+int main() {
   // printf("\n\n\t ***Memory Test ***\n\n");
   // printf("\n\n\t test name %s \n:", test_name[test]);
-
 #ifdef VERBOSE
   printf("\n\n");
   printf("  #############################################\n");
